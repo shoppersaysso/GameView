fa23a58c85138b076fb2a2c9c1a1fecb6f5d802b
Sara Hopper <sara.hopper@gmail.com>
2017-08-22 13:53:08 -0400

    setting up homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'application#index'
+  get '/home', to: 'application#home', as: 'home'
 
 
   resources :reviews
   resources :consoles
   resources :genres
   resources :games
+
+  get '/users/sign_out', to: 'application#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
