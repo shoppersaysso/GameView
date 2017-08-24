197b4248d49ca84ea50cae0146a33bca57e01696
Sara Hopper <sara.hopper@gmail.com>
2017-08-24 16:44:21 -0400

    new, create, show, edit, update, and destroy working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,44 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  # before_action :set_review, only: [:edit, :update, :destroy]
 
   # GET /reviews
   # GET /reviews.json
   def index
+    @review = Review.find(params[:game_id])
     @reviews = Review.all
   end
 
   # GET /reviews/1
   # GET /reviews/1.json
   def show
-    @game = Review.find(params[:game_id])
+    @review = Review.find(params[:id])
+    @game = Game.find(params[:game_id])
     @review.game_id = @game.id
+  #   if params[:game_id]
+  #    @game = Game.find_by(id: params[:game_id])
+  #    @review = @game.reviews.find_by(id: params[:id])
+  #    if @game.nil?
+  #      redirect_to game_reviews_path(@game), alert: "Review not found"
+  #    end
+  #  else
+  #    @review = Review.find(params[:id])
+  #  end
   end
 
   # GET /reviews/new
   def new
     @game = Game.find(params[:game_id])
     @review = @game.reviews.new
-
+  #   if params[:game_id] && !Game.exists?(params[:game_id])
+  #    redirect_to games_path, alert: "Game not found."
+  #  else
+  #    @review = Review.new(game_id: params[:game_id])
+  #   end
   end
 
   # GET /reviews/1/edit
   def edit
+    @review = Review.find(params[:game_id])
     @game = Game.find(params[:id])
   end
 
@@ -33,6 +49,8 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     @review.game_id = @game.id
+    current_user.gameview_level += 1
+    current_user.save
 
     respond_to do |format|
     if @review.save
@@ -48,6 +66,8 @@ class ReviewsController < ApplicationController
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
+    @review = Review.find(params[:id])
+    @game = Game.find(params[:game_id])
     respond_to do |format|
       if @review.update(review_params)
         format.html { redirect_to game_review_path(@review, @review.game_id), notice: 'Review was successfully updated.' }
@@ -62,18 +82,22 @@ class ReviewsController < ApplicationController
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
+    @review = Review.find(params[:game_id])
     @review.destroy
+    current_user.gameview_level -= 1
+    current_user.save
+    
     respond_to do |format|
-      format.html { redirect_to user_home_path, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to home_path, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:game_id])
-    end
+    # def set_review
+    #   @review = Review.find(params[:game_id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   end
 
   get '/users/sign_out', to: 'application#index'
+  delete '/games/:game_id/reviews/:id', to: 'reviews#destroy', as: 'delete_review'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
