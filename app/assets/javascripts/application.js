f8baaf84eea6612b17c2da01a702f3c3139b5262
Sara Hopper <sara.hopper@gmail.com>
2017-10-11 14:37:45 -0400

    fixed log in issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require_bootstrap
-//= require jquery
+//= require gameindex
---
+++ app/assets/javascripts/gameindex.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#showSignUp").click(function(){
+        $("#login").addClass("hidden");
+        $("#signup").removeClass("hidden");
+
+    });
+    $("#showLogIn").click(function(){
+        $("#login").removeClass("hidden");
+        $("#signup").addClass("hidden");
+
+    });
+});
